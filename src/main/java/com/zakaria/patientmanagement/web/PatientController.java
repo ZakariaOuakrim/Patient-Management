4c7e2b875c8383fb557dde7cf8f10d34d508df73
ZakariaOuakrim <zakarialukyguy@gmail.com>
2025-03-23 15:54:36 +0000

    delete patients

--- src/main/java/com/zakaria/patientmanagement/web/PatientController.java
+++ src/main/java/com/zakaria/patientmanagement/web/PatientController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -34,4 +35,11 @@ public class PatientController {
 
         return "patients";
     }
+
+    @GetMapping("/delete")
+    public String delete(Long id){
+        patientRepositoy.deleteById(id);
+        return "redirect:/index";
+    }
+
 }
