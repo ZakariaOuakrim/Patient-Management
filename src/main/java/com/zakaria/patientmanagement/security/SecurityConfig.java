95451e497e4c0206a81898e12e7dc070ecb51acb
ZakariaOuakrim <zakarialukyguy@gmail.com>
2025-04-19 21:28:54 +0100

    security filter chain & InMemoryUserDetails

--- src/main/java/com/zakaria/patientmanagement/security/SecurityConfig.java
+++ src/main/java/com/zakaria/patientmanagement/security/SecurityConfig.java
@@ -4,16 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        //we used the {noop} prefix to indicate that the password is stored in plain text and not encoded
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("{noop}1234").roles("USER").build(),
+                User.withUsername("user2").password("{noop}1234").roles("USER").build(),
+                User.withUsername("admin").password("{noop}1234").roles("USER","ADMIN").build()
+        );
+    }
+
+
+    // This method configures the security filter chain for the application.
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.formLogin();
         httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
         return httpSecurity.build();
     }
+
+    
 
 }
